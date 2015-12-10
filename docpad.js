8360913616628f8945b61b76063e73bc4afe9927
xavier <xavier@gandillot.com>
2015-12-10 21:53:55 +0100

    Preliminary i18n architecture

--- docpad.js
+++ docpad.js
@@ -7,17 +7,40 @@ module.exports = {
   checkVersion : true,
   collections : {},
   templateData : {
-        site: {
-            url : "MyURLConfig",
-            title:"MyTitleConfig ..."
-        }
-      },
+          site: {
+                url : "MyURLConfig",
+                title:"MyTitleConfig ..."
+                },
+          toEnglish : toEnglish,
+          toFrench : toFrench
+        },
   enabledPlugins : {
         marked : true,
         coffeescript:true,
         partials:true,
         eco:true
-  }
-
+        },
+  plugins:{
+        multilang: {
+          languages : ["fr","en"]
+          }
+        }
 
 };
+
+/****************** helper functions *****************************/
+
+// Being provided with a document, it returns the url to the same doc in English
+function toEnglish(doc) {
+  if(!doc) return "#";
+  var s = "/" + doc.relativeOutPath; // there is no leading "/" when called directly ...
+  return s.replace("/fr/","/en/");
+}
+
+
+// Being provided with a document, it returns the url to the same doc in French
+function toFrench(doc) {
+  if(!doc) return "#";
+  var s = "/" + doc.relativeOutPath; // there is no leading "/" when called directly ...
+  return s.replace("/en/","/fr/");
+}
